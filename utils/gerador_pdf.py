1360e08825f452bc7135c6cfdca751bc11c17b4d
Aciem <43767240+Aciem22@users.noreply.github.com>
2025-08-11 09:34:35 -0300

    Update gerador_pdf.py

--- utils/gerador_pdf.py
+++ utils/gerador_pdf.py
@@ -17,7 +17,7 @@ class GeradorPDF:
         c.drawString(50, 770, f"Transportadora: {self.dados['transportadora']}")
         c.drawString(50, 750, f"Motorista: {self.dados['motorista']}")
         c.drawString(50, 730, f"CPF:{self.dados['cpf']}")
-        c.drawString(50, 710, f"Número do Pedido: {self.dados['pedido']}")
+        c.drawString(50, 710, f"Número da Nota: {self.dados['pedido']}")
         c.drawString(50, 690, f"Data da Retirada: {self.dados['data'].strftime('%d/%m/%Y')}")
 
         assinatura_path = "assinatura_temp.png"
